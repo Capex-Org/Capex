2368f4939b7c04843ddde6d097e563defb59f7ac
avishka.g <avishka.g@datalogiclabs.com>
2025-08-24 13:42:06 +0530

    added shancn componet con

--- capex-web/vite.config.ts
+++ capex-web/vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from "vite";
+// vite.config.ts
+import { defineConfig, type PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwind()] as PluginOption[],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
